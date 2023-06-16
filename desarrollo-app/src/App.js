407b3df12e3f80c841276eb6a3c2dd5ea4a6a3fa
Debi414 <debi.alvaradobiran@gmail.com>
2023-06-16 18:19:52 +0200

    Added fetch API funtionality

--- desarrollo-app/src/App.js
+++ desarrollo-app/src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+  return React.createElement('h1', null, 'Hello World!');
 }
 
 export default App;
--- desarrollo-app/src/components/Routing.js
+++ desarrollo-app/src/components/Routing.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/HomePage";
 import About from "./pages/About";
 import Reservation from "./pages/BookingPage";
 import Order from "./pages/Order";
